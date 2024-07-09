a95547353de6eb8cc05b85e3a14ba7ceb21f6a88
TML45 <tassio.melo45@gmail.com>
2024-07-09 16:36:45 -0300

    feat: add base project structure

---
+++ src/main/java/com/e3gsix/fiap/tech_challenge_4_delivery_logistics/DeliveryLogisticsApplication.java
@@ -0,0 +1,13 @@
+package com.e3gsix.fiap.tech_challenge_4_delivery_logistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryLogisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryLogisticsApplication.class, args);
+	}
+
+}
